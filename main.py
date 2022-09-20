d092a55c39a5e0837ffc6991b5671b3c7c545dc1
LukeErlandson-11 <826619@mystma.org>
2022-09-20 14:58:53 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+p = input("Type password here\n")
+
+def password():
+    if p== "Knights19":
+        print ("Access Granted")
+    else:
+        print("Access Denied")
+
+password()
+
